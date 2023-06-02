d660b077bfa8792592b8083528da85c0f1ed36ee
M Awais Khalil <awaiskhalil.pk@gmail.com>
2023-06-02 23:56:55 +0500

    Add files via upload

---
+++ cucumber-html-report.js
@@ -0,0 +1,30 @@
+const { ParserOptions } = require('fast-csv');
+const { reporters } = require('mocha');
+const report = require('multiple-cucumber-html-reporter');
+
+report.generate({
+	jsonDir: 'cypress/cucumber-json',
+	reportPath: './reports/cucumber-htmlreport.html',
+	metadata:{
+        browser: {
+            name: 'chrome',
+            version: '60'
+        },
+        device: 'Local test machine',
+        platform: {
+            name: 'ubuntu',
+            version: '16.04'
+        }
+    },
+    customData: {
+        title: 'Run info',
+        data: [
+            {label: 'Project', value: 'Custom project'},
+            {label: 'Release', value: '1.2.3'},
+            {label: 'Cycle', value: 'B11221.34321'},
+            {label: 'Execution Start Time', value: 'May 19th 2023, 02:31 PM EST'},
+            {label: 'Execution End Time', value: 'May 19th 2023, 02:56 PM EST'}
+        ]
+    }
+});
+reporters.generate(options);
